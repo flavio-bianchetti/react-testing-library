48ac73e3dfd2832a478b03c2c7b59ba0c0c27ce6
Flávio Bianchetti <fjb.developer@gmail.com>
2021-11-06 16:06:26 -0300

    Atendido requisito 4 - validado via npm

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe(
+
+  'Teste o componente "NotFound" se...',
+  () => {
+    beforeEach(() => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/pagina/que-nao-existe/');
+    });
+
+    test(
+
+      'se página contém um heading h2 com o texto "Page requested not found 😭".',
+      () => {
+        const heading = screen.getByRole(
+          'heading',
+          { name: /page requested not found/i },
+        );
+        expect(heading).toBeInTheDocument();
+      },
+    );
+
+    test(
+
+      'se página mostra a imagem https://media.giphy.com/media/'
+      + 'kNSeTs31XBZ3G/giphy.gif.',
+      () => {
+        const image = screen.getAllByRole('img');
+        expect(image[1]).toHaveAttribute(
+          'src',
+          'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+        );
+      },
+    );
+  },
+);
